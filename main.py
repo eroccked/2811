4ea7eae449a7f804d7b15f9f37cf6cc2cabe0cca
Taras <eroccked@gmail.com>
2022-03-20 13:39:47 +0200

    les2 v0.2  с к-сью

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 class Student:
-    print("HI")
+    amount_of_students = 0
     def __init__(self, height=111):
         self.height = height
-        print("I am here!")
+        Student.amount_of_students += 1
 
-student1 = Student(4645)
-print(student1.height)
+student1 = Student()
+student2 = Student()
+print(student1.amount_of_students)
+student3 = Student()
+student4 = Student()
+student5 = Student()
+print(student4.amount_of_students)
+print(Student.amount_of_students)
