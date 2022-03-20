c0d6ced686d3badacdeaa577a21e2c5e9a3e3492
Taras <eroccked@gmail.com>
2022-03-20 13:12:51 +0200

    les2 v0.1

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Слава Україні")
+class Student:
+    print("HI")
+    def __init__(self, height=111):
+        self.height = height
+        print("I am here!")
+
+student1 = Student(4645)
+print(student1.height)
